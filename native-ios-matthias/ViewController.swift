392c5b8a1be92b98d81e6c09ffc6e7d5ca2eb5c3
M Vanooteghem <mvootegh@ptr-fyxp6d0gomoamqtgznr.18120a2.ip6.access.telenet.be>
2017-12-21 11:27:40 +0100

    Initial Commit

---
+++ native-ios-matthias/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  native-ios-matthias
+//
+//  Created by M Vanooteghem on 21/12/17.
+//  Copyright © 2017 M Vanooteghem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
